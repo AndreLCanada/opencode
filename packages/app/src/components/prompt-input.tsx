d4b0f2f296a8d1d1fc31744c27a7de76c171662d
Andre <andrelacketech@gmail.com>
2026-07-17 08:19:12 -0700

    fix(app): add min-w-[44px] to queue target and collapse model name on resize

--- packages/app/src/components/prompt-input.tsx
+++ packages/app/src/components/prompt-input.tsx
@@ -2377,7 +2377,7 @@ function ModelControlContent(props: { state: ComposerModelControlState; v2?: boo
           />
         )}
       </Show>
-      <span class="truncate leading-4">{props.state.modelName}</span>
+      <span data-component="model-name" class="min-w-0 truncate leading-4">{props.state.modelName}</span>
       <span class={props.v2 ? "-ml-0.5 -mr-1 flex shrink-0" : "-ml-1 shrink-0 flex size-fit"}>
         <Icon name="chevron-down" size="small" class="text-v2-icon-icon-muted" />
       </span>
